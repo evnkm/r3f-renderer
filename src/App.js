13bc85dd685566914049ffdd108271524640ed6d
Evan Kim <ekim2339@gmail.com>
2023-08-23 17:58:05 -0400

    cleaned

--- src/App.js
+++ src/App.js
@@ -50,22 +50,6 @@ function MovingCameraView(props) {
   );
 }
 
-// function TestCamHelper() {
-//   const [ref, camera] = useResource()
-//   return (
-//     <>
-//       <perspectiveCamera
-//         ref={ref}
-//         aspect={1200 / 600}
-//         radius={(1200 + 600) / 4}
-//         fov={45}
-//         position={[0, 0, 2]}
-//         onUpdate={self => self.updateProjectionMatrix()}
-//       />
-//       {camera && <cameraHelper args={camera} />}
-//     </>
-//   )
-
 export default function App() {
   return (
     <Canvas>
